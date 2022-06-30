e18e07274d67c569cfcdfd1cbf86f6faefc8f6d7
aric4381 <aricyohanes@amikom.ac.id>
2022-06-30 12:02:52 +0700

    registrasi

--- src/menu.h
+++ src/menu.h
@@ -1,15 +1,15 @@
 #pragma once
 #include <iostream>
 #include "gameLogic.h"
+#include <stdlib.h>
 using namespace std;
 
-int main()
+void StartMenu()
 {
     int pil;
-    char pil2;
+    int pil2;
     do
     {
-        menu:
         system("cls");
         cout << "=======================" << endl;
         cout << "       MAIN MENU       " << endl;
@@ -21,11 +21,43 @@ int main()
         cout << "Inputkan pilihan: "; cin >> pil;
         if(pil == 1)
         {
-
+            StartGame();
+            cout << "Tekan enter untuk kembali ke menu" << endl;
+            system("pause");
+            continue;
         }
         else if(pil == 2)
         {
-
+            do
+            {
+                system("cls");
+                cout << "Nama Player1 = " << player1.name << endl;
+                cout << "Nama Player2 = " << player2.name << endl << endl;
+                cout << "Tekan 1 untuk mengganti nama Player1" << endl;
+                cout << "Tekan 2 untuk mengganti nama Player2" << endl;
+                cout << "Tekan 0 untuk EXIT" << endl;
+                cout << "Inputkan pilihan: "; cin >> pil2;
+                if(pil2 == 1)
+                {
+                    cout << "Silahkan masukan nama Player1(Maks 8 karakter): ";
+                    cin.ignore(); 
+                    getline(cin, player1.name);
+                    system("pause");
+                } else if (pil2 == 2)
+                {   
+                    cout << "Silahkan masukan nama Player2(Maks 8 karakter): "; 
+                    cin.ignore();
+                    getline(cin, player2.name);
+                    system("pause");
+                } else
+                {
+                    cout << "Pilihan tidak tersedia!" << endl;
+                    system("pause");
+                }
+                
+                
+            } while (pil2 != 0);
+            
         }
         else if(pil == 3)
         {
